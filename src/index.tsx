ad111b38c35e3946173ed83a8d78fc0acb56f419
Evgeniya Timofeeva <timofeeva.zhenya.1995@gmail.com>
2024-07-11 00:23:20 +0200

    Initial commit: Copy

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import './reset.css'
+import { App } from './app/App'
+
+const rootElement = document.getElementById('root')
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  )
+} else {
+  console.error('Root element not found.')
+}
---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Button = styled.button<{ width?: string }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: ${({ width }) => width || '120px'};
+  height: 30px;
+  border-radius: 5px;
+  border: 1px solid gray;
+
+  &:hover {
+    background-color: white;
+    cursor: pointer;
+  }
+`
